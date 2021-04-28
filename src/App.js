267accc31ec1d5f07bcfb9696abf7949cdfe1cf1
yonglee <yonglee@student.42seoul.kr>
2021-04-28 16:39:23 +0900

    06-1 영화 API 사용해 보기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
     state = {
         isLoading: true,
         movies: [],
     };
+    getMovies = async () => {
+        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        console.log(movies);
+    }
     componentDidMount() {
-        // 영화 데이터 로딩!
-        setTimeout(() => {
-        this.setState({isLoading: false});
-        }, 6000);
+        this.getMovies();
     }
     render() {
         const { isLoading } = this.state;
