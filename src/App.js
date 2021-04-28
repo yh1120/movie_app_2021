1a61e4e6a6f5f58fd7a6fa6490f48c0983f4a5a0
yonglee <yonglee@student.42seoul.kr>
2021-04-28 14:39:27 +0900

    05 state와 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-import React from "react";
+import React from 'react';
 
 class App extends React.Component {
     state = {
-        count: 0,
+        isLoading: true,
+        movies: [],
     };
-
-    add = () => {
-        this.setState(current => ({
-            count: current.count + 1
-        }));
-    };
-
-    minus = () => {
-        this.setState(current => ({
-            count: current.count - 1
-        }));
-    };
-
+    componentDidMount() {
+        // 영화 데이터 로딩!
+        setTimeout(() => {
+        this.setState({isLoading: false});
+        }, 6000);
+    }
     render() {
-        return (
-            <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-            )
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
     }
 }
 
